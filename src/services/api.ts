170960444e98c9aaaf21cd759df9271470fc6716
sksayril <sksayril123@gmail.com>
2026-01-22 23:55:46 +0530

    chnages

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://apiloantrix.seotube.in/api';
-const API_BASE_URL = 'http://localhost:5009/api';
+const API_BASE_URL = 'https://apiloantrix.seotube.in/api';
+// const API_BASE_URL = 'http://localhost:5009/api';
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('adminToken');
